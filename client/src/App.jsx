b3fa4f0250d3ae0a2b8520607e144fbd5d6e2741
ayushman-03 <119766348+ayushman-03@users.noreply.github.com>
2024-11-29 02:28:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 const cookies = new Cookies();
 
-const apiKey = 'jnjf5ybuckax';
+const apiKey = 'zsptj27aehtc';
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
